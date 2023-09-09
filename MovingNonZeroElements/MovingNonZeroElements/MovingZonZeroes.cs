df8fec7044c6e99e71f049857f663e3a66fe40d4
Surajit Ghosh <Surajit.1986@gmail.com>
2023-09-09 12:10:42 +0530

    Moving non zeroes to the left of the array

---
+++ MovingNonZeroElements/MovingNonZeroElements/MovingZonZeroes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingNonZeroElements
+{
+    internal class MovingZonZeroes
+    {
+        public void MovingNonZeroesToLeft(int[] arr)
+        {
+            int i = 0;
+
+            foreach (int x in arr)
+            {
+                if(x!=0)
+                {
+                    arr[i++] = x;
+                }
+            }
+            while (i < arr.Length)
+            {
+                arr[i++] = 0;
+            }
+
+            Console.WriteLine("Output Array:");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MovingNonZeroElements/MovingNonZeroElements/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MovingNonZeroElements;
+
+Console.WriteLine("Hello, World!");
+
+MovingZonZeroes obj = new MovingZonZeroes();
+obj.MovingNonZeroesToLeft(new int[] { 0,1,0,3,12});
